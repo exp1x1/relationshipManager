d852352a9c9d562bc48bb5917e34f97dfd9d1be6
exp1x1 <gorvkrtianliya@gmail.com>
2025-04-27 16:40:03 +0530

    init firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'relationship-manager';
+
+  firestore: Firestore = inject(Firestore);
+  items$: Observable<any[]>;
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'items')
+    this.items$ = collectionData(aCollection);
+
+    console.log(this.items$, 'items$');
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "relationshipmanager-dcd9b", appId: "1:217851361598:web:724469dfcf647072be146f", storageBucket: "relationshipmanager-dcd9b.firebasestorage.app", apiKey: "AIzaSyB_1BtdVljCLLXvDOov_p5vdZtF3XpuDQQ", authDomain: "relationshipmanager-dcd9b.firebaseapp.com", messagingSenderId: "217851361598", measurementId: "G-6CFJWGCD0S" })), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideFirebaseApp(() => initializeApp({ projectId: "relationshipmanager-dcd9b", appId: "1:217851361598:web:724469dfcf647072be146f", storageBucket: "relationshipmanager-dcd9b.firebasestorage.app", apiKey: "AIzaSyB_1BtdVljCLLXvDOov_p5vdZtF3XpuDQQ", authDomain: "relationshipmanager-dcd9b.firebaseapp.com", messagingSenderId: "217851361598", measurementId: "G-6CFJWGCD0S" })), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions())]
 };
